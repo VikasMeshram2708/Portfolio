d32207111c238f5a3a57f3f4ed042a8fc7be2481
VikasMeshram2708 <meshramvikas744@gmail.com>
2026-02-10 19:35:49 +0530

    add: footer.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={`${inter.className} dark antialiased`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/footer.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { motion } from "motion/react";
+import Container from "./Container";
+import { ArrowUpRightIcon } from "lucide-react";
+const socialsData = [
+  {
+    label: "X",
+    url: "https://x.com/mevikas1008",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/sudo_dnf_upgrade",
+  },
+  {
+    label: "LinkedIN",
+    url: "https://www.linkedin.com/in/vikas-meshram",
+  },
+  {
+    label: "Github",
+    url: "https://github.com/VikasMeshram2708",
+  },
+] as const;
+export default function Footer() {
+  return (
+    <footer className="w-full">
+      <Container className="px-4 py-6">
+        <div className="flex items-center gap-8 text-sm">
+          {socialsData.map((data) => (
+            <motion.a
+              key={data.label}
+              href={data.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              initial="rest"
+              whileHover="hover"
+              animate="rest"
+              className="group flex items-center gap-2"
+            >
+              {/* Label */}
+              <motion.span
+                variants={{
+                  rest: { x: 0 },
+                  hover: { x: 2 },
+                }}
+                transition={{ duration: 0.2, ease: "easeOut" }}
+              >
+                {data.label}
+              </motion.span>
+              {/* Arrow */}
+              <motion.span
+                variants={{
+                  rest: { opacity: 0, x: -6 },
+                  hover: { opacity: 1, x: 0 },
+                }}
+                transition={{ duration: 0.2, ease: "easeOut" }}
+                className="flex items-center"
+              >
+                <ArrowUpRightIcon className="size-4" />
+              </motion.span>
+            </motion.a>
+          ))}
+        </div>
+        <div className="mt-4 border-t" />
+        <p className="mt-5 text-center text-neutral-400">
+          @Vikas Meshram, 2026
+        </p>
+      </Container>
+    </footer>
+  );
+}
